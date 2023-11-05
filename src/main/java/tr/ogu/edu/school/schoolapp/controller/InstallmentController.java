7fc8697cdf7af6864a75161404c835285200a3c3
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2023-11-06 00:50:28 +0300

    Installment ile ilgili unutulan temel yapılar oluşturuldu

--- src/main/java/tr/ogu/edu/school/schoolapp/controller/InstallmentController.java
+++ src/main/java/tr/ogu/edu/school/schoolapp/controller/InstallmentController.java
@@ -1,11 +1,47 @@
 package tr.ogu.edu.school.schoolapp.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import tr.ogu.edu.school.schoolapp.model.Installment;
+import tr.ogu.edu.school.schoolapp.service.InstallmentService;
 
 @RestController
-@AllArgsConstructor
+@RequestMapping("/installments")
+@RequiredArgsConstructor
 public class InstallmentController {
 
+	private final InstallmentService installmentService;
+
+	@GetMapping
+	public ResponseEntity<List<Installment>> getAllInstallments() {
+		return ResponseEntity.ok(installmentService.getAllInstallments());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Installment> getInstallmentById(@PathVariable Long id) {
+		return installmentService.getInstallmentById(id).map(ResponseEntity::ok)
+				.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public ResponseEntity<Installment> saveInstallment(@RequestBody Installment installment) {
+		Installment savedInstallment = installmentService.saveInstallment(installment);
+		return ResponseEntity.ok(savedInstallment);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteInstallment(@PathVariable Long id) {
+		installmentService.deleteInstallment(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/tr/ogu/edu/school/schoolapp/service/InstallmentService.java
+++ src/main/java/tr/ogu/edu/school/schoolapp/service/InstallmentService.java
@@ -1,5 +1,33 @@
 package tr.ogu.edu.school.schoolapp.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import tr.ogu.edu.school.schoolapp.model.Installment;
+import tr.ogu.edu.school.schoolapp.repository.InstallmentRepository;
+
+@Service
+@RequiredArgsConstructor
 public class InstallmentService {
 
+	private final InstallmentRepository installmentRepository;
+
+	public List<Installment> getAllInstallments() {
+		return installmentRepository.findAll();
+	}
+
+	public Optional<Installment> getInstallmentById(Long id) {
+		return installmentRepository.findById(id);
+	}
+
+	public Installment saveInstallment(Installment installment) {
+		return installmentRepository.save(installment);
+	}
+
+	public void deleteInstallment(Long id) {
+		installmentRepository.deleteById(id);
+	}
 }
