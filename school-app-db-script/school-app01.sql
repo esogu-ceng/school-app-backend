109c96a4af346f5a6b532463acca14a656d14228
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2024-02-01 00:27:59 +0300

    karakter sayısı güncellendi

--- school-app-db-script/school-app01.sql
+++ school-app-db-script/school-app01.sql
@@ -1,2 +1,2 @@
-ALTER TABLE public."user" ALTER COLUMN password TYPE character varying(60);
+ALTER TABLE public."user" ALTER COLUMN password TYPE character varying(72);
 ALTER TABLE "user" RENAME TO users;
