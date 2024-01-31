9294f5d7375f6491fa9d2a477841d28646912948
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2024-01-31 11:59:28 +0300

    tüm girişlere onay verildi

--- src/main/java/tr/ogu/edu/school/schoolapp/security/SecurityConfig.java
+++ src/main/java/tr/ogu/edu/school/schoolapp/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:19006"));
+		configuration.setAllowedOrigins(Arrays.asList("*"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
 		configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
