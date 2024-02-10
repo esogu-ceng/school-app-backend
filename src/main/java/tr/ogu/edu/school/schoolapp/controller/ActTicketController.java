f99592d51025343462796edb69ecd5210dbf5c70
Yasin <yasingok580@gmail.com>
2024-02-10 20:16:49 +0300

    Kullanıcının pdf'i görebilmesi için endpoint eklendi

--- src/main/java/tr/ogu/edu/school/schoolapp/controller/ActTicketController.java
+++ src/main/java/tr/ogu/edu/school/schoolapp/controller/ActTicketController.java
@@ -1,14 +1,21 @@
 package tr.ogu.edu.school.schoolapp.controller;
 
 import java.io.FileNotFoundException;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpHeaders;
+import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import tr.ogu.edu.school.schoolapp.exception.TicketRequiredException;
 import tr.ogu.edu.school.schoolapp.model.ActTicket;
 import tr.ogu.edu.school.schoolapp.service.ActTicketService;
 
@@ -22,5 +29,20 @@ public class ActTicketController {
     public ResponseEntity<String> createActTicket(@RequestBody ActTicket actTicket) throws FileNotFoundException {
         actTicketService.createActTicket(actTicket);
         return ResponseEntity.ok("Bilet başarıyla oluşturuldu.");
+    }
+
+    // Bileti çekmemizi sağlayan endpoint
+    @GetMapping("/ticket/{verificationCode}")
+    public ResponseEntity<Resource> getTicketPdf(@PathVariable String verificationCode) throws IOException {
+        try {
+            Resource resource = actTicketService.getTicketPdfResource(verificationCode);    // Dosya kaynağını almak için
+            return ResponseEntity.ok().
+                contentType(MediaType.APPLICATION_PDF).
+                header(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=" + verificationCode + ".pdf").
+                body(resource);
+        } catch (TicketRequiredException e) {
+            // TODO: handle exception
+            throw new TicketRequiredException(e);
+        }
     }
 }
