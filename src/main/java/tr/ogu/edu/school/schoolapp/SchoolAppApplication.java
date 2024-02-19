75289a2fef62ac3aa99e8780c430e31952eddf41
mukrime.sagiroglu <mukrime.sagiroglu@avkaryazilim.com.tr>
2024-02-19 10:39:20 +0300

    Cors Hatası Çözüldü

--- src/main/java/tr/ogu/edu/school/schoolapp/SchoolAppApplication.java
+++ src/main/java/tr/ogu/edu/school/schoolapp/SchoolAppApplication.java
@@ -5,11 +5,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableAsync
 @ComponentScan(basePackages = { "tr.ogu.edu.school.schoolapp.*" })
 public class SchoolAppApplication {
+	@Profile("dev")
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:19006");
+			}
+		};
+	}
 
 	@Bean
 	public ModelMapper modelMapper() {
